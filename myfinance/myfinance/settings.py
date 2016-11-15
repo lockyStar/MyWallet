dc2fe9366cffed63b4996c92065b3683f9832bc6
LockyStar <Zhabrevalexander@yandex.ru>
2016-11-15 17:37:52 +0300

    templates

--- myfinance/myfinance/settings.py
+++ myfinance/myfinance/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myfinance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql_psycopg2'),
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
