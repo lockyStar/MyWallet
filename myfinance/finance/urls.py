9da8543d61798bccaafff4dd1aec80c121bdcb4b
LockyStar <Zhabrevalexander@yandex.ru>
2016-11-16 23:06:00 +0300

    charges name added

--- myfinance/finance/urls.py
+++ myfinance/finance/urls.py
@@ -4,6 +4,6 @@ from finance.views import home,add_charge,account_status,random_example
 urlpatterns = [
     url(r'^$', home),
     url(r'^charges/$', random_example),
-    url(r'^charges/(?P<account_id>\d{1,16})/$', account_status),
+    url(r'^charges/(?P<account_id>\d{1,16})/$', account_status, name='status'),
     url(r'^addcharge/(?P<account_id>\d{1,16})/$', add_charge, name='add_charge')
     ]
