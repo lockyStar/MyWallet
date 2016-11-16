68ae4c91bba13170996d19cf7fb92c29aa8035a7
LockyStar <Zhabrevalexander@yandex.ru>
2016-11-16 15:05:55 +0300

    Account, Charge models

--- myfinance/finance/models.py
+++ myfinance/finance/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+
+    name = models.CharField(max_length=100)
+    total = models.DecimalField(decimal_places=2)
+    account_number = models.BigIntegerField()
+
+    class Meta:
+        db_table = 'accounts'
+
+
+class Charge(models.Model):
+    value = models.DecimalField(decimal_places=2)
+    date = models.DateField()
+    category = models.CharField(max_length=100)
+    purpose = models.CharField(max_length=150)
+    account_id = models.ForeignKey(Account, related_name='account_number')
+
+    class Meta:
+        db_table = 'charges'
